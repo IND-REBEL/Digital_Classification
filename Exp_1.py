32f125940f439bc1686079c4a9cb2529470a66a2
Ujjwal Singh <d23csa004@iitj.ac.in>
2023-10-31 04:47:41 +0530

    ASSpgnment3

--- Exp_1.py
+++ Exp_1.py
@@ -12,68 +12,39 @@ hand-written digits, from 0-9.
 # License: BSD 3 clause
 
 # Import datasets, classifiers and performance metrics
-from sklearn import  metrics, svm
-from sklearn.model_selection import train_test_split
-from utils import preprocess_data, split_data, train_model, read_digits
+from sklearn import metrics, svm
 
+from utils import preprocess_data, split_data, train_model, read_digits, predict_and_eval, train_test_dev_split, get_hyperparameter_combinations, tune_hparams
 
-#1 get data sets
-X, y =read_digits()
+# 1. Get the dataset
+X, y = read_digits()
 
-# data spliting
-X_train, X_test, y_train, y_test = split_data(X, y, test_size=0.3);
+# 2. Hyperparameter combinations
+# 2.1. SVM
+gamma_list = [0.001, 0.01, 0.1, 1]
+C_list = [1, 10, 100, 1000]
+h_params={}
+h_params['gamma'] = gamma_list
+h_params['C'] = C_list
+h_params_combinations = get_hyperparameter_combinations(h_params)
 
-# data preprocessing
-X_train= preprocess_data(X_train)
-X_test= preprocess_data(X_test)
+test_sizes =  [0.1, 0.2, 0.3, 0.45]
+dev_sizes  =  [0.1, 0.2, 0.3, 0.45]
+for test_size in test_sizes:
+    for dev_size in dev_sizes:
+        train_size = 1- test_size - dev_size
+        # 3. Data splitting -- to create train and test sets                
+        X_train, X_test, X_dev, y_train, y_test, y_dev = train_test_dev_split(X, y, test_size=test_size, dev_size=dev_size)
+        # 4. Data preprocessing
+        X_train = preprocess_data(X_train)
+        X_test = preprocess_data(X_test)
+        X_dev = preprocess_data(X_dev)
+    
+        best_hparams, best_model, best_accuracy  = tune_hparams(X_train, y_train, X_dev, 
+        y_dev, h_params_combinations)
 
+        test_acc = predict_and_eval(best_model, X_test, y_test)
+        train_acc = predict_and_eval(best_model, X_train, y_train)
+        dev_acc = best_accuracy
 
-
-#model trainning
-model = train_model(X_train, y_train, {'gamma': 0.001}, model_type="svm")
-
-
-# Predict the value of the digit on the test subset
-predicted = model.predict(X_test)
-
-
-
-"""_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, predicted):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title(f"Prediction: {prediction}")"""
-
-
-
-print(
-    f"Classification report for classifier {model}:\n"
-    f"{metrics.classification_report(y_test, predicted)}\n"
-)
-
-
-
-disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-disp.figure_.suptitle("Confusion Matrix")
-print(f"Confusion matrix:\n{disp.confusion_matrix}")
-
-
-
-
-# The ground truth and predicted lists
-y_true = []
-y_pred = []
-cm = disp.confusion_matrix
-
-# For each cell in the confusion matrix, add the corresponding ground truths
-# and predictions to the lists
-for gt in range(len(cm)):
-    for pred in range(len(cm)):
-        y_true += [gt] * cm[gt][pred]
-        y_pred += [pred] * cm[gt][pred]
-
-print(
-    "Classification report rebuilt from confusion matrix:\n"
-    f"{metrics.classification_report(y_true, y_pred)}\n"
-)
+        print("test_size={:.2f} dev_size={:.2f} train_size={:.2f} train_acc={:.2f} dev_acc={:.2f} test_acc={:.2f}".format(test_size, dev_size, train_size, train_acc, dev_acc, test_acc))
