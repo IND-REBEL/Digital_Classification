5e61c415548c3c00b4005c2bdad144cfab3c3597
Ujjwal Singh <d23csa004@iitj.ac.in>
2023-10-31 06:27:26 +0530

    2 test for hperam

---
+++ test_utils.py
@@ -0,0 +1,26 @@
+from utils import get_hyperparameter_combinations, train_test_dev_split,read_digits, tune_hparams, preprocess_data
+
+def test_for_hparam_cominations_count():
+    # a test case to check that all possible combinations of paramers are indeed generated
+    gamma_list = [0.001, 0.01, 0.1, 1]
+    C_list = [1, 10, 100, 1000]
+    h_params={}
+    h_params['gamma'] = gamma_list
+    h_params['C'] = C_list
+    h_params_combinations = get_hyperparameter_combinations(h_params)
+
+    assert len(h_params_combinations) == len(gamma_list) * len(C_list)
+
+def test_for_hparam_cominations_count_values():
+    # a test case to check that all possible combinations of paramers are indeed generated
+    gamma_list = [0.001, 0.01]
+    C_list = [1]
+    h_params={}
+    h_params['gamma'] = gamma_list
+    h_params['C'] = C_list
+    h_params_combinations = get_hyperparameter_combinations(h_params)
+
+    expected_param_combo_1 = {'gamma': 0.001, 'C': 1}
+    expected_param_combo_2 = {'gamma': 0.01, 'C': 10}
+
+    assert (expected_param_combo_1 in h_params_combinations) and (expected_param_combo_2 in h_params_combinations)
